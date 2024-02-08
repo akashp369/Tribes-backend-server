bdf2a3c5bd2fff20c61c865b7977de93e60169ae
akashp369 <aakashprajapat59@gmail.com>
2024-02-08 16:34:24 +0530

    update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const auth_controller = require("../controllers/auth.controller");
 
 router.post("/admin/signup", auth_controller.adminSignup_post);
 router.post("/admin/signin", auth_controller.adminSignin_post);
+router.post("/admin/updatepassword", auth_controller.updatePassword);
 router.post("/user/signup", auth_controller.userSignup_post);
 router.post("/user/signin", auth_controller.userSignin_post);
 router.post('/user/forgot/sendotp', auth_controller.forgot)
